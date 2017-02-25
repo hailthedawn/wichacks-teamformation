37f7a5d27953ecfe331313771fecd97dd539734f
Daria Chaplin <dxc4643@g.rit.edu>
2017-02-25 16:53:49 -0500

    added functionality to Individuals class

--- Individuals.java
+++ Individuals.java
@@ -1,5 +1,16 @@
 /**
  * @author: Ketaki Tiak kkt7778@rit.edu
  */
+
+import java.util.HashSet;
+
 public class Individuals {
+    private HashSet _individuals;
+    
+    public Individuals()
+    {
+        individuals = new HashSet<Individual>();
+    }
+    
+    public void addIndividual()
 }
