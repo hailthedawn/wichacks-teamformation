b6190adc8e7d93b9214950558cf91bdc4d466069
Daria Chaplin <dxc4643@g.rit.edu>
2017-02-25 18:50:52 -0500

    added search function to match individuals

--- Individuals.java
+++ Individuals.java
@@ -2,7 +2,7 @@
  * @author: Ketaki Tiak kkt7778@rit.edu
  */
 
-import java.util.HashSet;
+import java.util.*;
 
 public class Individuals {
     private HashSet individuals;
@@ -28,5 +28,41 @@ public class Individuals {
     public int numIndividuals()
     {
         return individuals.size();
+    }
+    
+    //creates a score for each individual based on matching skills
+    public ArrayList<Team> searchIndividuals(ArrayList<String> checkLanguages, boolean ifCompeting, boolean hasIdea, boolean isIdeaFinal)
+    {
+        //creates hashmap
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        
+        for(Individual person: individuals)
+        {
+            int score = 0;
+            //checks if any languages are equal
+            for(String lang: person.getLangs())
+            {
+                if(checkLanguages.contains(lang))
+                { score++; }
+            }
+            //checks if they have equal competition intentions
+            if((Individual.getIfCompeting() && ifCompeting) || (!Individual.getIfCompeting() && !ifCompeting))
+            { score++; }
+            //checks if their hasIdea is equal
+            if((Individual.isHasIdea() && hasIdea) || (!Individual.isHasIdea() && !hasIdea))
+            {
+                score++;
+                
+                //checks whether isIdeaFinal is equal
+                if (hasIdea)
+                {
+                    if ((Individual.isIdeaFinal() && isIdeaFinal) || (!Individual.isIdeaFinal() && !isIdeaFinal))
+                    { score++; }
+                }
+            }
+            
+            //attaches score to the person
+            map.put(person, score);
+        }
     }
 }
